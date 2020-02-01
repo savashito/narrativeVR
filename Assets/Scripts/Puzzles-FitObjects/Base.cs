3fe7792dd23ba2fbb3250263dbf8ec144fba3fd1
Neodemuse <lechuga.ochoa.daniel@gmail.com>
2020-02-01 16:08:35 -0600

    Add files via upload

---
+++ Assets/Scripts/Puzzles-FitObjects/Base.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+    public int completo;
+    public GameObject mensaje;
+    int piezas;
+    private void Start()
+    {
+        piezas = 0;
+    }
+    
+
+    public void AñadirPieza() {
+        piezas = piezas + 1;
+        if (piezas == completo) {
+            mensaje.SetActive(true);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Puzzles-FitObjects/Piezas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piezas : MonoBehaviour
+{
+    public GameObject padre;
+    public string tag;
+    GameObject pieza;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == tag) {
+            Debug.Log("Chocando");           
+            pieza = other.transform.parent.gameObject;
+            pieza.transform.parent = padre.transform;
+            /*Vector3 distancia = pieza.transform.position - padre.transform.position;
+            Debug.Log("Posicion Base:" + padre.transform.position);
+            Debug.Log("Posicion Pieza:" + pieza.transform.position);
+            Debug.Log("Distancia:" + distancia);
+            Collider caja = padre.GetComponent<Collider>();
+
+            pieza.transform.position = pieza.transform.position - distancia + caja.bounds.size; */
+            DragObject script = pieza.GetComponent<DragObject>();
+            Destroy(script);
+            Base comportamiento = padre.GetComponent<Base>();
+            comportamiento.AñadirPieza();
+            Destroy(this);
+        }
+    }
+
+}
