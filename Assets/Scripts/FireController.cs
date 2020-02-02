0b27d648610501ca096bdb339961d47835e53e70
Thomas Kearney <tom.f.kearney@gmail.com>
2020-02-01 20:21:24 -0600

    Add first version fireplace controller without interaction

---
+++ Assets/Scripts/FireController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour
+{
+    public GameObject flame;
+    private AudioSource flameSoundSource;
+    void Start()
+    {
+        flame.SetActive(false);
+        flameSoundSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        //check for collision with candle
+        // if(collision)
+        // {
+        //     StartFlame();
+        // }
+    }
+
+    private void StartFlame()
+    {
+        flame.SetActive(true);
+        flameSoundSource.Play();
+    }
+}
