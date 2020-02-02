b819b0486f5c132c12a365c1bd75c6c4673d7742
Thomas Kearney <tom.f.kearney@gmail.com>
2020-02-01 19:56:31 -0600

    Add AudioManager code and game object to scene

--- Assets/Scripts/narrativeVRAudioManager.cs
+++ Assets/Scripts/narrativeVRAudioManager.cs
@@ -5,9 +5,67 @@ using UnityEngine;
 public class narrativeVRAudioManager : MonoBehaviour
 {
   public AudioClip afterCandleLightAudio;
+  public AudioClip afterPutPictureTogetherAudio;
+  public AudioClip afterPutDrumTogetherAudio;
+  public AudioClip afterPutRockingHorseTogetherAudio;
+  public AudioClip afterPutPaintBrushTogetherAudio;
+  public AudioClip afterSeeFullPhotoAudio;
+  
+//SORRY FOR NAMING THESE LIKE A LITERAL CRAZY PERSON!!!
+
+  private AudioSource source;
+
+  public void Start()
+  {
+    source = GetComponent<AudioSource>();
+    if(!source)
+    {
+      Debug.LogError("Awake no audiosource on manager");
+    }
+  }
 
   public void PlayAfterCandleLightAudio()
   {
-      
+    PlayAudio(afterCandleLightAudio);
   }
+
+  public void PlayAfterPutPictureTogetherAudio()
+  {
+    PlayAudio(afterPutPictureTogetherAudio);
+  }
+ 
+  public void PlayAfterPutDrumTogetherAudio()
+  {
+    PlayAudio(afterPutDrumTogetherAudio);
+  }
+ 
+  public void PlayAfterPutRockingHorseTogetherAudio()
+  {
+    PlayAudio(afterPutRockingHorseTogetherAudio);
+  }
+ 
+  public void PlayAfterPutPaintBrushTogetherAudio()
+  {
+    PlayAudio(afterPutPaintBrushTogetherAudio);
+  }
+ 
+  public void PlayAfterSeeFullPhotoAudio()
+  {
+    PlayAudio(afterSeeFullPhotoAudio);
+  }
+
+  private void PlayAudio(AudioClip clip)
+  {
+    source.clip = clip;
+    source.Play();
+  }
+
+  // void Update()
+  // {
+  //   if(Input.GetKeyDown(KeyCode.A))
+  //   {
+  //     PlayAfterCandleLightAudio();
+  //   }
+  // }
+  
 }
