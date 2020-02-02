1563c492cc6c0158bb97968d42dc5d969e1f667f
savashito <rodrigosavage@gmail.com>
2020-02-02 01:08:10 -0600

    jojo

---
+++ Assets/DebugController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugController : MonoBehaviour
+{
+    public Text t;
+
+    public void Debug (string s)
+    {
+        t.text += s + "\n";
+    }
+    public void Clear()
+    {
+        t.text = "";
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/PieceController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceController : MonoBehaviour
+{
+    public DebugController d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        d.Clear();
+        d.Debug("Piece Controller started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
